69bea105c88c4a3c1a2c9ba27aef03d9a5e1ca7b
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-30 02:33:42 +0900

    feat: getMedicineRemindersメソッドを更新し、曜日を型安全に管理するためのプライベートメソッドを追加

--- reminder_cron/src/service/api/medicines/medicines_api.ts
+++ reminder_cron/src/service/api/medicines/medicines_api.ts
@@ -1,8 +1,9 @@
 import { ApiCore, Response } from "../core/api_core";
 
+type reminderDayOfWeek = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday";
 export interface MedicineRemindersRequest {
     reminderTime: string;
-    reminderDayOfWeek: string;
+    reminderDayOfWeek: reminderDayOfWeek;
 }
 
 export class MedicinesApi extends ApiCore {
@@ -10,7 +11,40 @@ export class MedicinesApi extends ApiCore {
         super({ endpoint: '/medicines' });
     }
 
-    async getMedicineReminders({ reminderTime, reminderDayOfWeek }: MedicineRemindersRequest): Promise<Response> {
-        return await this.get('/reminders?reminderTime=' + reminderTime + '&reminderDayOfWeek=' + reminderDayOfWeek);
+    /**
+     * 時刻と曜日に一致するおくすりリマインダーを取得する
+     * @param date 
+     * @returns 
+     */
+    async getMedicineReminders(date: Date): Promise<Response> {
+        const request: MedicineRemindersRequest = this._makeReminderRequest(date);
+        return await this.get('/reminders?reminderTime=' + request.reminderTime + '&reminderDayOfWeek=' + request.reminderDayOfWeek);
+    }
+
+    private _makeReminderRequest(date: Date): MedicineRemindersRequest {
+        const reminderTime = date.getHours() + ':' + date.getMinutes();
+        const reminderDayOfWeek = this._getDayOfWeek(date);
+        return { reminderTime, reminderDayOfWeek };
+    }
+
+    private _getDayOfWeek(date: Date): reminderDayOfWeek {
+        switch (date.getDay()) {
+            case 0:
+                return 'sunday';
+            case 1:
+                return 'monday';
+            case 2:
+                return 'tuesday';
+            case 3:
+                return 'wednesday';
+            case 4:
+                return 'thursday';
+            case 5:
+                return 'friday';
+            case 6:
+                return 'saturday';
+            default:
+                throw new Error('Invalid day of week');
+        }
     }
 }
