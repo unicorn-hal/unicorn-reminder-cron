32c1b2f107ceca00d09f1421b48285481012348b
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-30 02:02:47 +0900

    feat: MedicineApi実装

---
+++ reminder_cron/src/service/api/medicines/medicines_api.ts
@@ -0,0 +1,16 @@
+import { ApiCore, Response } from "../core/api_core";
+
+export interface MedicineRemindersRequest {
+    reminderTime: string;
+    reminderDayOfWeek: string;
+}
+
+export class MedicinesApi extends ApiCore {
+    constructor() {
+        super('/medicines');
+    }
+
+    async getMedicineReminders({ reminderTime, reminderDayOfWeek }: MedicineRemindersRequest): Promise<Response> {
+        return await this.get('/reminders?reminderTime=' + reminderTime + '&reminderDayOfWeek=' + reminderDayOfWeek);
+    }
+}
